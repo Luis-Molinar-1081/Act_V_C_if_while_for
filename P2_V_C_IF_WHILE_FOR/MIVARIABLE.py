2fc9cfb1a57af58da2bf1a3f440f5cbad8e52d18
Luis-Molinar-1081 <164534066+Luis-Molinar-1081@users.noreply.github.com>
2024-08-30 12:20:48 -0600

    Add files via upload

---
+++ P2_V_C_IF_WHILE_FOR/MIVARIABLE.py
@@ -0,0 +1,7 @@
+# Variables
+print("Mis variables")
+
+edad=17
+nombre="Luis Carlos Molinar Cuellar"
+print("Edad: ",edad)
+print("Nombre ",nombre)
---
+++ P2_V_C_IF_WHILE_FOR/parafor.py
@@ -0,0 +1,5 @@
+# for en español es para 
+print("ciclo for de repeticion")
+minovias=["Lupita","Ana","Agripina"]
+for chicas in minovias:
+    print(chicas)
